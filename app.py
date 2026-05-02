5f32932d0ad460305bdb8186daa9c0465556b57a
Alex Titus Paul <alextpaul144@gmail.com>
2026-05-02 23:48:59 +0530

    .

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app.secret_key = 'health_records_secret_key_2026'
 DB_CONFIG = {
     'host': 'localhost',
     'user': 'root',
-    'password': '@Axtp8589',        # Update with your MySQL password
+    'password': '',        # Update with your MySQL password
     'database': 'health_records_db'
 }
 
